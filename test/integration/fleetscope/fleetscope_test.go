0067b1c7230f655d92a2fd4e94ab176bd4be08bb
Renato Rudnicki <renatojr@ciandt.com>
2024-03-20 15:38:15 -0300

    integration tests for fleetscope

--- test/integration/fleetscope/fleetscope_test.go
+++ test/integration/fleetscope/fleetscope_test.go
@@ -19,7 +19,9 @@ import (
 	"testing"
 	"time"
 
+	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/gcloud"
 	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/tft"
+	"github.com/stretchr/testify/assert"
 	"github.com/terraform-google-modules/enterprise-application/test/integration/testutils"
 )
 
@@ -40,12 +42,27 @@ func TestFleetscope(t *testing.T) {
 			vars := map[string]interface{}{
 				"fleet_project_id":       multitenant.GetStringOutput("fleet_project_id"),
 				"cluster_membership_ids": multitenant.GetStringOutputList("cluster_membership_ids"),
+				//"clusters_ids":           multitenant.GetStringOutputList("clusters_ids"),
+				//"environment":            multitenant.GetStringOutput("env"),
 			}
+
 			fleetscope := tft.NewTFBlueprintTest(t,
 				tft.WithTFDir(fmt.Sprintf("../../../4-fleetscope/envs/%s", envName)),
 				tft.WithVars(vars),
 				tft.WithRetryableTerraformErrors(testutils.RetryableTransientErrors, 3, 2*time.Minute),
 			)
+
+			fleetscope.DefineVerify(func(assert *assert.Assertions) {
+				fleetscope.DefaultVerify(assert)
+
+				// GKE Scope
+				fleetProjectID := vars["fleet_project_id"].(string)
+				clustersMembership := fleetscope.GetStringOutputList("cluster_membership_ids")
+				membershipID := gcloud.Runf(t, "container hub memberships describe projects/%[1]s/locations/us-central1/memberships/cluster-us-central1-%[2]s --project=%[1]s", fleetProjectID, envName)
+				opmembershipID := fmt.Sprintf("//gkehub.googleapis.com/%s", membershipID.Get("name").String())
+				assert.Equal(clustersMembership[0], opmembershipID, fmt.Sprintf("membership ID should be %s", clustersMembership[0]))
+			})
+
 			fleetscope.Test()
 		})
 	}
