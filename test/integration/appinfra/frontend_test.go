b8fc7fe8171945b63b0fb702bbae8bc69aab762c
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2024-05-14 13:12:33 -0300

    chore: split app test from infra test (#128)

--- test/integration/appinfra/frontend_test.go
+++ test/integration/appinfra/frontend_test.go
@@ -16,22 +16,18 @@ package frontend
 
 import (
 	"fmt"
-	"os"
 	"strings"
 	"testing"
 	"time"
 
 	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/gcloud"
-	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/git"
 	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/tft"
 	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/utils"
 	"github.com/stretchr/testify/assert"
 	"github.com/terraform-google-modules/enterprise-application/test/integration/testutils"
-
-	cp "github.com/otiai10/copy"
 )
 
-func TestAppinfraFrontend(t *testing.T) {
+func TestAppInfraFrontend(t *testing.T) {
 	multitenant := tft.NewTFBlueprintTest(t, tft.WithTFDir("../../../2-multitenant/envs/development"))
 	multitenant_nonprod := tft.NewTFBlueprintTest(t, tft.WithTFDir("../../../2-multitenant/envs/non-production"))
 	multitenant_prod := tft.NewTFBlueprintTest(t, tft.WithTFDir("../../../2-multitenant/envs/production"))
@@ -57,10 +53,7 @@ func TestAppinfraFrontend(t *testing.T) {
 		serviceName := "frontend"
 		applicationName := "cymbal-bank"
 		repoName := fmt.Sprintf("eab-%s-%s", applicationName, serviceName)
-		appRepo := fmt.Sprintf("https://source.developers.google.com/p/%s/r/%s", projectID, repoName)
 		region := "us-central1"
-		pipelineName := "frontend"
-		prodTarget := "dev"
 
 		prj := gcloud.Runf(t, "projects describe %s", projectID)
 		projectNumber := prj.Get("projectNumber").String()
@@ -220,95 +213,6 @@ func TestAppinfraFrontend(t *testing.T) {
 		ciServiceAccountPath := fmt.Sprintf("projects/%s/serviceAccounts/%s", projectID, ciServiceAccountEmail)
 		buildTriggerOp := gcloud.Runf(t, "builds triggers describe %s --project %s --region %s", buildTriggerName, projectID, region)
 		assert.Equal(ciServiceAccountPath, buildTriggerOp.Get("serviceAccount").String(), fmt.Sprintf("cloud build trigger %s should have service account %s", buildTriggerName, ciServiceAccountPath))
-
-		// Push cymbal bank app source code
-		tmpDirApp := t.TempDir()
-		gitApp := git.NewCmdConfig(t, git.WithDir(tmpDirApp))
-		gitAppRun := func(args ...string) {
-			_, err := gitApp.RunCmdE(args...)
-			if err != nil {
-				t.Fatal(err)
-			}
-		}
-
-		gitAppRun("clone", "--branch", "v0.6.4", "https://github.com/GoogleCloudPlatform/bank-of-anthos.git", tmpDirApp)
-		gitAppRun("config", "user.email", "eab-robot@example.com")
-		gitAppRun("config", "user.name", "EAB Robot")
-		gitAppRun("config", "--global", "credential.https://source.developers.google.com.helper", "gcloud.sh")
-		gitAppRun("config", "--global", "init.defaultBranch", "main")
-		gitAppRun("config", "--global", "http.postBuffer", "157286400")
-		gitAppRun("checkout", "-b", "main")
-		gitAppRun("remote", "add", "google", appRepo)
-		datefile, err := os.OpenFile(tmpDirApp+"/src/frontend/date.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer datefile.Close()
-
-		_, err = datefile.WriteString(time.Now().String() + "\n")
-		if err != nil {
-			t.Fatal(err)
-		}
-		gitAppRun("rm", "-r", "src/components")
-		gitAppRun("rm", "-r", "src/frontend/k8s")
-		err = cp.Copy("../../../6-appsource/cymbal-bank/components", fmt.Sprintf("%s/src/components", tmpDirApp))
-		if err != nil {
-			t.Fatal(err)
-		}
-		err = cp.Copy("../../../6-appsource/cymbal-bank/frontend/skaffold.yaml", fmt.Sprintf("%s/src/frontend/skaffold.yaml", tmpDirApp))
-		if err != nil {
-			t.Fatal(err)
-		}
-		err = cp.Copy("../../../6-appsource/cymbal-bank/frontend/k8s", fmt.Sprintf("%s/src/frontend/k8s", tmpDirApp))
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		gitAppRun("add", ".")
-		gitApp.CommitWithMsg("initial commit", []string{"--allow-empty"})
-		gitAppRun("push", "--all", "google", "-f")
-
-		lastCommit := gitApp.GetLatestCommit()
-		// filter builds triggered based on pushed commit sha
-		buildListCmd := fmt.Sprintf("builds list --region=%s --filter substitutions.COMMIT_SHA='%s' --project %s", region, lastCommit, projectID)
-		// poll build until complete
-		pollCloudBuild := func(cmd string) func() (bool, error) {
-			return func() (bool, error) {
-				build := gcloud.Runf(t, cmd).Array()
-				if len(build) < 1 {
-					return true, nil
-				}
-				latestWorkflowRunStatus := build[0].Get("status").String()
-				if latestWorkflowRunStatus == "SUCCESS" {
-					return false, nil
-				}
-				return true, nil
-			}
-		}
-		utils.Poll(t, pollCloudBuild(buildListCmd), 40, 30*time.Second)
-		releaseListCmd := fmt.Sprintf("deploy releases list --project=%s --delivery-pipeline=%s --region=%s --filter=name:%s", projectID, pipelineName, region, lastCommit[0:7])
-		releases := gcloud.Runf(t, releaseListCmd).Array()
-		if len(releases) == 0 {
-			t.Fatal("Failed to find the release")
-		}
-		releaseName := releases[0].Get("name")
-		fmt.Println(releaseName)
-		rolloutListCmd := fmt.Sprintf("deploy rollouts list --project=%s --delivery-pipeline=%s --region=%s --release=%s --filter targetId=%s-%s", projectID, pipelineName, region, releaseName, pipelineName, prodTarget)
-		// Poll CD rollouts until rollout is successful
-		pollCloudDeploy := func(cmd string) func() (bool, error) {
-			return func() (bool, error) {
-				rollouts := gcloud.Runf(t, cmd).Array()
-				if len(rollouts) < 1 {
-					return true, nil
-				}
-				latestRolloutState := rollouts[0].Get("state").String()
-				if latestRolloutState == "SUCCEEDED" {
-					return false, nil
-				}
-				return true, nil
-			}
-		}
-		utils.Poll(t, pollCloudDeploy(rolloutListCmd), 30, 60*time.Second)
 	})
 	frontend.Test()
 }
