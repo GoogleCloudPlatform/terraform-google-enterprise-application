f4f3681d5c0de9836981819fa1fbf570798f872b
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2024-05-23 15:22:56 -0300

    Tests parallel for app

--- test/integration/appsource/cymbal_bank_test.go
+++ test/integration/appsource/cymbal_bank_test.go
@@ -48,14 +48,13 @@ func TestSourceCymbalBank(t *testing.T) {
 	)
 
 	region := multitenant.GetStringOutputList("cluster_regions")[0]
-	for appName, serviceNames := range testutils.ServicesNames {
-		appSourcePath := fmt.Sprintf("../../../6-appsource/%s", appName)
-		appFactory := tft.NewTFBlueprintTest(t, tft.WithTFDir(fmt.Sprintf("../../../3-appfactory/apps/%s", appName)))
 
-		for _, serviceName := range serviceNames {
-			t.Run(fmt.Sprintf("%s/%s", appName, serviceName), func(t *testing.T) {
-				t.Parallel()
-
+	t.Run("AppSource", func(t *testing.T) {
+		t.Parallel()
+		for appName, serviceNames := range testutils.ServicesNames {
+			appSourcePath := fmt.Sprintf("../../../6-appsource/%s", appName)
+			appFactory := tft.NewTFBlueprintTest(t, tft.WithTFDir(fmt.Sprintf("../../../3-appfactory/apps/%s", appName)))
+			for _, serviceName := range serviceNames {
 				splitServiceName = strings.Split(serviceName, "-")
 				prefixServiceName = splitServiceName[0]
 				suffixServiceName = splitServiceName[len(splitServiceName)-1]
@@ -197,7 +196,7 @@ func TestSourceCymbalBank(t *testing.T) {
 				})
 				appsource.Test()
 
-			})
+			}
 		}
-	}
+	})
 }
