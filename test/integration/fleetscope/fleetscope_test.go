5740f669f6f5286def833e65601bafef2905300c
Andrew Peabody <andrewpeabody@google.com>
2024-06-05 18:11:13 -0700

    Update fleetscope_test.go

--- test/integration/fleetscope/fleetscope_test.go
+++ test/integration/fleetscope/fleetscope_test.go
@@ -105,8 +105,8 @@ func TestFleetscope(t *testing.T) {
 						{
 							assert.Equal("MANAGEMENT_AUTOMATIC", gkeFeatureOp.Get("fleetDefaultMemberConfig.mesh.management").String(), fmt.Sprintf("Hub Feature %s should have mesh menagement equal to MANAGEMENT_AUTOMATIC", feature))
 
+							// TODO: validate cluster service mesh state
 							// for _, id := range clusterIds {
-								// TODO: validate cluster service mesh state
 								//assert.Equal("MANAGEMENT_AUTOMATIC", gkeFeatureOp.Get(fmt.Sprintf("membershipSpecs.projects/*/locations/*/memberships/%s.mesh.management", id)).String(), fmt.Sprintf("Cluster %s should have mesh menagement equal to MANAGEMENT_AUTOMATIC", id))
 								//assert.Equal("OK", gkeFeatureOp.Get(fmt.Sprintf("membershipStates.projects/*/locations/*/memberships/%s.state.code", id)).String(), fmt.Sprintf("Cluster %s should have servicemesh state code equal to OK", id))
 							// }
