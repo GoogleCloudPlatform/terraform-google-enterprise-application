5ff5db9081f2cc6776e772cfa56cc305d39f9552
caetano-colin <ccolin@ciandt.com>
2024-08-26 08:51:37 -0300

    add params to dev and nonprod

--- 2-multitenant/envs/development/main.tf
+++ 2-multitenant/envs/development/main.tf
@@ -27,6 +27,7 @@ module "env" {
   folder_id           = var.envs[local.env].folder_id
   network_project_id  = var.envs[local.env].network_project_id
   billing_account     = var.envs[local.env].billing_account
+  network_self_link   = var.envs[local.env].network_self_link
   cluster_subnetworks = [var.envs[local.env].subnets_self_links[0]]
   cluster_type        = "AUTOPILOT"
 }
