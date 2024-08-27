3ca183972bf10c3b06a6c61841bacfbe3e50a57e
caetano-colin <ccolin@ciandt.com>
2024-08-27 17:19:24 -0300

    fix lint issues

--- 2-multitenant/envs/development/main.tf
+++ 2-multitenant/envs/development/main.tf
@@ -26,7 +26,7 @@ module "env" {
   org_id              = var.envs[local.env].org_id
   folder_id           = var.envs[local.env].folder_id
   network_project_id  = var.envs[local.env].network_project_id
-  billing_account     = var.envs[local.env].billing_account  
+  billing_account     = var.envs[local.env].billing_account
   cluster_subnetworks = [var.envs[local.env].subnets_self_links[0]]
   cluster_type        = "AUTOPILOT"
 }
