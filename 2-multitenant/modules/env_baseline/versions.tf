92ec3aea2cb14b89f30d37cd5b9987c14e51ae3a
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2024-09-26 18:08:02 -0300

    fix: fixes provider_metadata (#249)

--- 2-multitenant/modules/env_baseline/versions.tf
+++ 2-multitenant/modules/env_baseline/versions.tf
@@ -29,6 +29,6 @@ terraform {
   }
 
   provider_meta "google" {
-    module_name = "blueprints/terraform/terraform-google-enterprise-application:environments/v0.1.0"
+    module_name = "blueprints/terraform/terraform-google-enterprise-application:multitenant/v0.1.0"
   }
 }
