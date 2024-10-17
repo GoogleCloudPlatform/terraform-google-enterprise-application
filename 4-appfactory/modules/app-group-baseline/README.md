31c252dadc76de66edc1bed976f24d2db8f37f80
caetano-colin <ccolin@ciandt.com>
2024-10-17 15:35:59 -0300

    restore 4-appfactory

---
+++ 4-appfactory/modules/app-group-baseline/README.md
@@ -0,0 +1,37 @@
+<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| acronym | The acronym of the application. | `string` | n/a | yes |
+| admin\_project\_id | The admin project id associated with the microservice. This project will host resources like microservice CI/CD pipelines. If set, `create_admin_project` must be set to `false`. | `string` | n/a | yes |
+| billing\_account | Billing Account ID for application admin project resources. | `string` | n/a | yes |
+| bucket\_force\_destroy | When deleting a bucket, this boolean option will delete all contained objects. If false, Terraform will fail to delete buckets which contain objects. | `bool` | `false` | no |
+| bucket\_prefix | Name prefix to use for buckets created. | `string` | `"bkt"` | no |
+| cloudbuild\_sa\_roles | Optional to assign to custom CloudBuild SA. Map of project name or any static key to object with list of roles. Keys much match keys from var.envs | <pre>map(object({<br>    roles = list(string)<br>  }))</pre> | `{}` | no |
+| create\_admin\_project | Boolean value that indicates whether a admin project should be created for the microservice. | `bool` | n/a | yes |
+| create\_infra\_project | Boolean value that indicates whether an infrastructure project should be created for the microservice. | `bool` | n/a | yes |
+| envs | Environments | <pre>map(object({<br>    billing_account    = string<br>    folder_id          = string<br>    network_project_id = string<br>    network_self_link  = string<br>    org_id             = string<br>    subnets_self_links = list(string)<br>  }))</pre> | n/a | yes |
+| folder\_id | Folder ID of parent folder for application admin resources. If deploying on the enterprise foundation blueprint, this is usually the 'common' folder. | `string` | n/a | yes |
+| infra\_project\_apis | List of APIs to enable for environment-specific application infra projects | `list(string)` | <pre>[<br>  "iam.googleapis.com",<br>  "cloudresourcemanager.googleapis.com",<br>  "serviceusage.googleapis.com",<br>  "cloudbilling.googleapis.com"<br>]</pre> | no |
+| location | Location for build buckets. | `string` | `"us-central1"` | no |
+| org\_id | Google Cloud Organization ID. | `string` | n/a | yes |
+| service\_name | The name of a single service application. | `string` | `"demo-app"` | no |
+| tf\_apply\_branches | List of git branches configured to run terraform apply Cloud Build trigger. All other branches will run plan by default. | `list(string)` | <pre>[<br>  "development",<br>  "nonproduction",<br>  "production"<br>]</pre> | no |
+| trigger\_location | Location of for Cloud Build triggers created in the workspace. If using private pools should be the same location as the pool. | `string` | `"global"` | no |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| app\_admin\_project\_id | Project ID of the application admin project. |
+| app\_cloudbuild\_workspace\_apply\_trigger\_id | ID of the apply cloud build trigger. |
+| app\_cloudbuild\_workspace\_artifacts\_bucket\_name | Artifacts bucket name for the application workspace. |
+| app\_cloudbuild\_workspace\_logs\_bucket\_name | Logs bucket name for the application workspace. |
+| app\_cloudbuild\_workspace\_plan\_trigger\_id | ID of the plan cloud build trigger. |
+| app\_cloudbuild\_workspace\_state\_bucket\_name | Terraform state bucket name for the application workspace. |
+| app\_infra\_project\_ids | Application environment projects IDs. |
+| app\_infra\_repository\_name | Name of the application infrastructure repository. |
+| app\_infra\_repository\_url | URL of the application infrastructure repository. |
+
+<!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
---
+++ 4-appfactory/modules/app-group-baseline/variables.tf
@@ -0,0 +1,122 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+variable "service_name" {
+  type        = string
+  default     = "demo-app"
+  description = "The name of a single service application."
+}
+
+variable "acronym" {
+  type        = string
+  description = "The acronym of the application."
+  validation {
+    condition     = length(var.acronym) <= 3
+    error_message = "The max length for acronym is 3 characters."
+  }
+}
+
+variable "org_id" {
+  type        = string
+  description = "Google Cloud Organization ID."
+}
+
+variable "folder_id" {
+  type        = string
+  description = "Folder ID of parent folder for application admin resources. If deploying on the enterprise foundation blueprint, this is usually the 'common' folder."
+}
+
+variable "billing_account" {
+  type        = string
+  description = "Billing Account ID for application admin project resources."
+}
+
+variable "envs" {
+  type = map(object({
+    billing_account    = string
+    folder_id          = string
+    network_project_id = string
+    network_self_link  = string
+    org_id             = string
+    subnets_self_links = list(string)
+  }))
+  description = "Environments"
+}
+
+variable "infra_project_apis" {
+  type        = list(string)
+  description = "List of APIs to enable for environment-specific application infra projects"
+  default = [
+    "iam.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "serviceusage.googleapis.com",
+    "cloudbilling.googleapis.com",
+  ]
+}
+
+variable "cloudbuild_sa_roles" {
+  description = "Optional to assign to custom CloudBuild SA. Map of project name or any static key to object with list of roles. Keys much match keys from var.envs"
+  type = map(object({
+    roles = list(string)
+  }))
+  default = {}
+}
+
+variable "bucket_prefix" {
+  description = "Name prefix to use for buckets created."
+  type        = string
+  default     = "bkt"
+}
+
+variable "bucket_force_destroy" {
+  description = "When deleting a bucket, this boolean option will delete all contained objects. If false, Terraform will fail to delete buckets which contain objects."
+  type        = bool
+  default     = false
+}
+
+variable "location" {
+  description = "Location for build buckets."
+  type        = string
+  default     = "us-central1"
+}
+
+variable "trigger_location" {
+  description = "Location of for Cloud Build triggers created in the workspace. If using private pools should be the same location as the pool."
+  type        = string
+  default     = "global"
+}
+
+variable "tf_apply_branches" {
+  description = "List of git branches configured to run terraform apply Cloud Build trigger. All other branches will run plan by default."
+  type        = list(string)
+  default     = ["development", "nonproduction", "production"]
+}
+
+variable "admin_project_id" {
+  description = "The admin project id associated with the microservice. This project will host resources like microservice CI/CD pipelines. If set, `create_admin_project` must be set to `false`."
+  type        = string
+}
+
+variable "create_infra_project" {
+  description = "Boolean value that indicates whether an infrastructure project should be created for the microservice."
+  type        = bool
+}
+
+variable "create_admin_project" {
+  description = "Boolean value that indicates whether a admin project should be created for the microservice."
+  type        = bool
+}
+
