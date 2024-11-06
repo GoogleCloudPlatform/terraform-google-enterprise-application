20ee80fc93174f0a0de3e11a959f249ad68dc25f
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2024-11-06 14:47:48 -0300

    increase build wait time

--- test/integration/standalone_single_project/cymbal_bank_single_project_test.go
+++ test/integration/standalone_single_project/cymbal_bank_single_project_test.go
@@ -189,7 +189,7 @@ func TestSingleProjectSourceCymbalBank(t *testing.T) {
 						return true, nil
 					}
 				}
-				utils.Poll(t, pollCloudBuild(buildListCmd), 40, 30*time.Second)
+				utils.Poll(t, pollCloudBuild(buildListCmd), 40, 60*time.Second)
 				releaseListCmd := fmt.Sprintf("deploy releases list --project=%s --delivery-pipeline=%s --region=%s --filter=name:%s", servicesInfoMap[serviceName].ProjectID, servicesInfoMap[serviceName].ServiceName, region, lastCommit[0:7])
 				releases := gcloud.Runf(t, releaseListCmd).Array()
 				if len(releases) == 0 {
