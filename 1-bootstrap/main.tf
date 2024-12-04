b6578713b2acaa8938e5936026b118ae1bc51c2a
caetano-colin <ccolin@ciandt.com>
2024-12-04 11:18:48 -0300

    revert 1-bootstrap to google upstream

--- 1-bootstrap/main.tf
+++ 1-bootstrap/main.tf
@@ -17,49 +17,32 @@
 locals {
   cb_config = {
     "multitenant" = {
+      repo_name    = "eab-multitenant",
       bucket_infix = "mt"
       roles = [
         "roles/container.admin"
       ]
     }
     "applicationfactory" = {
+      repo_name    = "eab-applicationfactory",
       bucket_infix = "af"
       roles        = ["roles/resourcemanager.projectIamAdmin"]
     }
     "fleetscope" = {
+      repo_name    = "eab-fleetscope",
       bucket_infix = "fs"
       roles        = []
     }
   }
-  use_csr                    = var.cloudbuildv2_repository_config.repo_type == "CSR"
-  csr_repos                  = local.use_csr ? [for k, v in var.cloudbuildv2_repository_config.repositories : v.repository_name] : []
   cb_service_accounts_emails = { for k, v in module.tf_cloudbuild_workspace : k => reverse(split("/", v.cloudbuild_sa))[0] }
 }
 
 resource "google_sourcerepo_repository" "gcp_repo" {
-  for_each = toset(local.csr_repos)
+  for_each = local.cb_config
 
   project                      = var.project_id
-  name                         = each.value
+  name                         = each.value.repo_name
   create_ignore_already_exists = true
-}
-
-module "cloudbuild_repositories" {
-  count = local.use_csr ? 0 : 1
-  # TODO: update git URL to registry format after release is available 
-  source = "git::https://github.com/terraform-google-modules/terraform-google-bootstrap.git//modules/cloudbuild_repo_connection?ref=a6072e0db92d976f1535dab767ad6b4331bcb4ef"
-
-  project_id = var.project_id
-
-  credential_config = {
-    connection_type                             = var.cloudbuildv2_repository_config.repo_type
-    github_secret_id                            = var.cloudbuildv2_repository_config.github_secret_id
-    github_app_id_secret_id                     = var.cloudbuildv2_repository_config.github_app_id_secret_id
-    gitlab_read_authorizer_credential_secret_id = var.cloudbuildv2_repository_config.gitlab_read_authorizer_credential_secret_id
-    gitlab_authorizer_credential_secret_id      = var.cloudbuildv2_repository_config.gitlab_authorizer_credential_secret_id
-    gitlab_webhook_secret_id                    = var.cloudbuildv2_repository_config.gitlab_webhook_secret_id
-  }
-  cloud_build_repositories = var.cloudbuildv2_repository_config.repositories
 }
 
 module "tfstate_bucket" {
@@ -73,20 +56,19 @@ module "tfstate_bucket" {
 }
 
 module "tf_cloudbuild_workspace" {
-  for_each = var.cloudbuildv2_repository_config.repositories
+  source  = "terraform-google-modules/bootstrap/google//modules/tf_cloudbuild_workspace"
+  version = "~> 9.0"
 
-  # TODO: update git URL to registry format after release is available 
-  source = "git::https://github.com/terraform-google-modules/terraform-google-bootstrap.git//modules/tf_cloudbuild_workspace?ref=a6072e0db92d976f1535dab767ad6b4331bcb4ef"
-
+  for_each = local.cb_config
 
   project_id = var.project_id
   location   = var.location
 
-  tf_repo_uri           = local.use_csr ? google_sourcerepo_repository.gcp_repo[each.key].url : module.cloudbuild_repositories[0].cloud_build_repositories_2nd_gen_repositories[each.key].id
-  tf_repo_type          = local.use_csr ? "CLOUD_SOURCE_REPOSITORIES" : "CLOUDBUILD_V2_REPOSITORY"
+  tf_repo_uri           = google_sourcerepo_repository.gcp_repo[each.key].url
+  tf_repo_type          = "CLOUD_SOURCE_REPOSITORIES"
   trigger_location      = var.trigger_location
-  artifacts_bucket_name = "${var.bucket_prefix}-${var.project_id}-${local.cb_config[each.key].bucket_infix}-build"
-  log_bucket_name       = "${var.bucket_prefix}-${var.project_id}-${local.cb_config[each.key].bucket_infix}-logs"
+  artifacts_bucket_name = "${var.bucket_prefix}-${var.project_id}-${each.value.bucket_infix}-build"
+  log_bucket_name       = "${var.bucket_prefix}-${var.project_id}-${each.value.bucket_infix}-logs"
 
   create_state_bucket    = false
   state_bucket_self_link = module.tfstate_bucket.bucket.self_link
@@ -96,7 +78,7 @@ module "tf_cloudbuild_workspace" {
   cloudbuild_sa_roles = {
     "roles" = {
       project_id = var.project_id
-    roles = local.cb_config[each.key].roles }
+    roles = each.value.roles }
   }
 
   substitutions = {
