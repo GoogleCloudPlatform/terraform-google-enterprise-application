40852631087135b49a4e96c42c25cb9cbf30f362
caetano-colin <ccolin@ciandt.com>
2024-12-17 16:46:28 -0300

    adjusting shell script

--- test/setup/scripts/gitlab_self_hosted.sh
+++ test/setup/scripts/gitlab_self_hosted.sh
@@ -15,26 +15,28 @@ PROJECT_ID=$(curl http://metadata.google.internal/computeMetadata/v1/project/pro
 URL="https://$EXTERNAL_IP.nip.io"
 echo "external_url \"$URL\"" > /etc/gitlab/gitlab.rb && gitlab-ctl reconfigure
 
+
 # Wait for the server to handle authentication requests
-while [[ true ]]; do
+for i in {1..100}; do
   RESPONSE_BODY=$(curl $URL)
 
   if echo "$RESPONSE_BODY" | grep -q "You are .*redirected"; then
-      echo "GitLab initialized and ready to sign-in"
+      echo "GitLab initialized and ready to handle sign-in interactions"
+      sleep 5
       # Sign-in to gitlab
       GITLAB_INITIAL_PASSWORD=$(cat /etc/gitlab/initial_root_password  | grep "Password:" | awk '{ print $2 }')
       echo "grant_type=password&username=root&password=$GITLAB_INITIAL_PASSWORD" > auth.txt
       access_token=$(curl -k --data "@auth.txt" --request POST "$URL/oauth/token" | tee /tmp/token_curl_stdout.txt | jq -r '.access_token')
       if [[ $access_token == "null" ]]; then
-        echo "Authentication failed, will try again in 15 seconds. More information about the request:"
+        echo "Authentication failed, will reconfigure the gitlab server and try again. More information about the request:"
         cat /tmp/token_curl_stdout.txt
-        sleep 15
-        access_token=$(curl -k --data "grant_type=password&username=root&password=$GITLAB_INITIAL_PASSWORD" --request POST "$URL/oauth/token" | jq -r '.access_token')
+        gitlab-ctl reconfigure
+        continue
       fi
-      echo access_token=$(echo $access_token | head -c 6)*********
+      echo "access_token=$(echo $access_token | head -c 6)*********"
       # Create a personal access token and store in secret manager
       personal_token=$(curl -k --request POST --header "Authorization: Bearer $access_token" --data "name=mytoken" --data "scopes[]=api" --data "scopes[]=read_api" --data "scopes[]=read_user" "$URL/api/v4/users/1/personal_access_tokens" -k | jq -r '.token')
-      echo personal_token=$(echo $personal_token | head -c 6)*********
+      echo "personal_token=$(echo $personal_token | head -c 6)*********"
       printf $personal_token | gcloud secrets create gitlab-pat-from-vm --project=$PROJECT_ID --data-file=-
       break
   else
