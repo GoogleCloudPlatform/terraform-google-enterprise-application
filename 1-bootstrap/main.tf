8edca7fb3827728d6fd9886ea5f483fac9d0c1b9
caetano-colin <ccolin@ciandt.com>
2024-12-19 09:04:36 -0300

    lint fixes

--- 1-bootstrap/main.tf
+++ 1-bootstrap/main.tf
@@ -47,7 +47,8 @@ resource "google_sourcerepo_repository" "gcp_repo" {
 module "cloudbuild_repositories" {
   count = local.use_csr ? 0 : 1
 
-  source  = "git::https://github.com/caetano-colin/terraform-google-bootstrap.git//modules/cloudbuild_repo_connection?ref=use-parent-resource"
+  // TODO: update to upstream module after PR https://github.com/terraform-google-modules/terraform-google-bootstrap/pull/335 is merger
+  source = "git::https://github.com/caetano-colin/terraform-google-bootstrap.git//modules/cloudbuild_repo_connection?ref=use-parent-resource"
 
   project_id = var.project_id
 
--- test/setup/scripts/gitlab_self_hosted.sh
+++ test/setup/scripts/gitlab_self_hosted.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # GitLab Installation
 apt-get update
 apt-get install -y curl openssh-server ca-certificates tzdata perl jq
@@ -17,16 +31,16 @@ echo "external_url \"$URL\"" > /etc/gitlab/gitlab.rb && gitlab-ctl reconfigure
 
 # Wait for the server to handle authentication requests
 for i in {1..100}; do
-  RESPONSE_BODY=$(curl $URL)
+  RESPONSE_BODY=$(curl "$URL")
 
   if echo "$RESPONSE_BODY" | grep -q "You are .*redirected"; then
-      personal_token=$(cat /dev/random | tr -dc "[:alnum:]" | head -c 20)
+      personal_token=$(tr -dc "[:alnum:]" < /dev/random | head -c 20)
       gitlab-rails runner "token = User.find_by_username('root').personal_access_tokens.create(scopes: ['api', 'read_api', 'read_user'], name: 'Automation token', expires_at: 365.days.from_now); token.set_token('$personal_token'); token.save!"
-      echo "personal_token=$(echo $personal_token | head -c 6)*********"
-      printf $personal_token | gcloud secrets create gitlab-pat-from-vm --project=$PROJECT_ID --data-file=-
+      echo "personal_token=$(echo "$personal_token" | head -c 3)*********"
+      echo -n "$personal_token" | gcloud secrets create gitlab-pat-from-vm --project="$PROJECT_ID" --data-file=-
       break
   else
-      echo "GitLab is not ready for sign-in operations. Waiting 5 seconds and will try again."
+      echo "$i: GitLab is not ready for sign-in operations. Waiting 5 seconds and will try again."
       echo "Command Output:"
       echo "$RESPONSE_BODY"
       sleep 5
