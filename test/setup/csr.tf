c80ddac3ca9a0f30fb3756ae850753fdad39ab9d
caetano-colin <ccolin@ciandt.com>
2024-12-20 09:50:32 -0300

    create all repos

--- test/setup/csr.tf
+++ test/setup/csr.tf
@@ -1,8 +1,20 @@
+locals {
+  csr_repos = [
+    "eab-cymbal-bank-accounts-contacts",
+    "eab-cymbal-bank-accounts-userservice",
+    "eab-cymbal-bank-frontend",
+    "eab-cymbal-bank-ledger-balancereader",
+    "eab-cymbal-bank-ledger-ledgerwriter",
+    "eab-cymbal-bank-ledger-transactionhistory",
+    "eab-cymbal-shop-cymbalshop"
+  ]
+}
 # DEPRECATED - TODO: Remove after CSR support is removed
 resource "google_sourcerepo_repository" "app_repo" {
+  for_each = toset(local.csr_repos)
 
   project = local.project_id
-  name    = "eab-default-example-hello-world"
+  name    = each.key
 
   create_ignore_already_exists = true
 }
