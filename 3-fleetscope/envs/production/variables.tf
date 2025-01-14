6d875b3f420563c3d4d7a076fd9f028cfe233cd3
Caetano Colin <164910343+caetano-colin@users.noreply.github.com>
2025-01-14 15:36:34 -0300

    Update 3-fleetscope/envs/production/variables.tf
    Co-authored-by: Andrew Peabody <andrewpeabody@google.com>

--- 3-fleetscope/envs/production/variables.tf
+++ 3-fleetscope/envs/production/variables.tf
@@ -25,7 +25,7 @@ variable "remote_state_bucket" {
 }
 
 variable "config_sync_secret_type" {
-  description = "The type of Secret configured for access to the Config Sync Git repo. Must be `ssh`, `cookiefile`, `gcenode`, `gcpserviceaccount`, `githubapp`, `token`, or `none`. Depending on the credential type, additional steps must be executed prior to this step. Refer to the following documentation for guidance: https://cloud.google.com/kubernetes-engine/enterprise/config-sync/docs/how-to/installing-config-sync#git-creds-secret"
+  description = "The type of `Secret` configured for access to the Config Sync Git repo. Must be `ssh`, `cookiefile`, `gcenode`, `gcpserviceaccount`, `githubapp`, `token`, or `none`. Depending on the credential type, additional steps must be executed prior to this step. Refer to the following documentation for guidance: https://cloud.google.com/kubernetes-engine/enterprise/config-sync/docs/how-to/installing-config-sync#git-creds-secret"
   type        = string
   default     = "gcpserviceaccount"
 }
