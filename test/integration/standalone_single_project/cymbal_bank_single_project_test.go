1fb4790d0c48c957938c226b64d4c955bdb5def3
caetano-colin <ccolin@ciandt.com>
2025-01-29 11:26:23 -0300

    add project flag

--- test/integration/standalone_single_project/cymbal_bank_single_project_test.go
+++ test/integration/standalone_single_project/cymbal_bank_single_project_test.go
@@ -218,7 +218,7 @@ func TestSingleProjectSourceCymbalBank(t *testing.T) {
 							t.Logf("Build finished successfully %s. \n", build[0].Get("buildTriggerId"))
 							return false, nil
 						} else if latestWorkflowRunStatus == "FAILURE" {
-							logsCmd := fmt.Sprintf("builds log %s", build[0].Get("buildTriggerId").String())
+							logsCmd := fmt.Sprintf("builds log %s --project=%s", build[0].Get("buildTriggerId").String(), build[0].Get("projectId").String())
 							logs := gcloud.Runf(t, logsCmd).String()
 							t.Logf("%s ci-build-log: %s", servicesInfoMap[serviceName].ServiceName, logs)
 							return false, fmt.Errorf("Build failed %s.", build[0].Get("buildTriggerId"))
