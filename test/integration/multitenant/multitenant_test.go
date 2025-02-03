df8156e1834bf833c5699de6ffdc66bde5e062a9
Caetano Colin <164910343+caetano-colin@users.noreply.github.com>
2025-02-03 09:22:27 -0300

    chore: PR #336 follow-up - change if statement style (#350)
    Co-authored-by: Andrew Peabody <andrewpeabody@google.com>

--- test/integration/multitenant/multitenant_test.go
+++ test/integration/multitenant/multitenant_test.go
@@ -140,11 +140,8 @@ func TestMultitenant(t *testing.T) {
 								assert.False(pool.Get("autoscaling.autoprovisioned").Bool(), "NodePool autoscaling autoprovisioned should disabled(false)")
 							} else if regexp.MustCompile(`^nap-.*`).FindString(pool.Get("name").String()) != "" {
 								assert.True(pool.Get("autoscaling.autoprovisioned").Bool(), "NodePool autoscaling autoprovisioned should enabled(true)")
-							} else {
-								// exception for custom regional restricted arm64 node pool
-								if pool.Get("name").String() != "regional-arm64-pool" {
-									t.Fatalf("Error: unknown node pool: %s", pool.Get("name").String())
-								}
+							} else if pool.Get("name").String() != "regional-arm64-pool" {
+								t.Fatalf("Error: unknown node pool: %s", pool.Get("name").String())
 							}
 							// common to all valid node pools
 							assert.True(pool.Get("autoscaling.enabled").Bool(), "NodePool auto scaling should be enabled (true)")
