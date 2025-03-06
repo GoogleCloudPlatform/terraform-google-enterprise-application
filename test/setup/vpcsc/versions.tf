794f911421d2f2dc7297550d01b19f97357a3b05
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2025-03-06 16:15:53 -0300

    Enables terraform debug, pins max terraform version

--- test/setup/vpcsc/versions.tf
+++ test/setup/vpcsc/versions.tf
@@ -19,11 +19,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 6.6"
+      version = ">= 6.6, < 7"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "6.20.0"
+      version = "6.20.0, < 7"
     }
   }
 }
