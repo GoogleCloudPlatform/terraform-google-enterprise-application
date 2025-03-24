44faf135ff26c0d788ac447a8498f61738b9eb79
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2025-03-24 10:36:13 -0300

    Adds access level on hpc example, add cb and cd service agents on access level

--- examples/hpc/5-appinfra/hpc/hpc-team-b/envs/development/main.tf
+++ examples/hpc/5-appinfra/hpc/hpc-team-b/envs/development/main.tf
@@ -35,4 +35,5 @@ module "provision-monte-carlo-infra" {
   bucket_force_destroy     = var.bucket_force_destroy
   workerpool_id            = data.terraform_remote_state.bootstrap.outputs.cb_private_worker_pool_id
   team                     = "hpc-team-b"
+  access_level_name        = var.access_level_name
 }
