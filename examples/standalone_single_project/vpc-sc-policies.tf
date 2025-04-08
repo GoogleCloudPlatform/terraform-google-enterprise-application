e3a97319160dee2627e5e8f13a65563a3d1e838b
caetano-colin <ccolin@ciandt.com>
2025-04-08 10:10:54 -0300

    add more policies

--- examples/standalone_single_project/vpc-sc-policies.tf
+++ examples/standalone_single_project/vpc-sc-policies.tf
@@ -19,6 +19,11 @@ data "google_project" "workerpool_project" {
   project_id = local.worker_pool_project
 }
 
+
+###############################################
+#              EGRESS POLICIES                #
+###############################################
+
 resource "google_access_context_manager_service_perimeter_egress_policy" "egress_policy" {
   count     = var.service_perimeter_mode == "ENFORCE" ? 1 : 0
   perimeter = var.service_perimeter_name
@@ -92,6 +97,10 @@ resource "google_access_context_manager_service_perimeter_egress_policy" "servic
   }
 }
 
+###############################################
+#              INGRESS POLICIES               #
+###############################################
+
 resource "google_access_context_manager_service_perimeter_ingress_policy" "ingress_policy" {
   count     = var.service_perimeter_mode == "ENFORCE" ? 1 : 0
   perimeter = var.service_perimeter_name
@@ -133,6 +142,62 @@ resource "google_access_context_manager_service_perimeter_dry_run_ingress_policy
 
     operations {
       service_name = "cloudbuild.googleapis.com"
+      method_selectors {
+        method = "*"
+      }
+    }
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "google_access_context_manager_service_perimeter_ingress_policy" "cymbal_bank_private_deployment" {
+  count     = var.service_perimeter_mode == "ENFORCE" ? 1 : 0
+  perimeter = var.service_perimeter_name
+  ingress_from {
+    identity_type = "ANY_IDENTITY"
+    sources {
+      resource = "projects/${data.google_project.workerpool_project.number}"
+    }
+  }
+  ingress_to {
+    resources = [
+      "projects/${data.google_project.project.number}",
+    ]
+
+    operations {
+      service_name = "storage.googleapis.com"
+      method_selectors {
+        method = "*"
+      }
+    }
+    operations {
+      service_name = "logging.googleapis.com"
+      method_selectors {
+        method = "*"
+      }
+    }
+    operations {
+      service_name = "artifactregistry.googleapis.com"
+      method_selectors {
+        method = "*"
+      }
+    }
+    operations {
+      service_name = "clouddeploy.googleapis.com"
+      method_selectors {
+        method = "*"
+      }
+    }
+    operations {
+      service_name = "gkehub.googleapis.com"
+      method_selectors {
+        method = "*"
+      }
+    }
+    operations {
+      service_name = "connectgateway.googleapis.com"
       method_selectors {
         method = "*"
       }
