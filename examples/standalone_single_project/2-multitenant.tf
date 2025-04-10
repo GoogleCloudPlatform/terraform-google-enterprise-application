a994289566e7cb004aeb9a248c40bc00d18ff4e0
caetano-colin <ccolin@ciandt.com>
2025-04-10 18:36:12 -0300

    add depends on on 2-multitenant single_project

--- examples/standalone_single_project/2-multitenant.tf
+++ examples/standalone_single_project/2-multitenant.tf
@@ -46,4 +46,8 @@ module "multitenant_infra" {
   billing_account        = null
   service_perimeter_name = var.service_perimeter_name
   service_perimeter_mode = var.service_perimeter_mode
+
+  depends_on = [
+    google_access_context_manager_service_perimeter_egress_policy.service_directory_policy
+  ]
 }
