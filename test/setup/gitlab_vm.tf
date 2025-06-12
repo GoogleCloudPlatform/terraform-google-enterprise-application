42d9c5c2d6f1210bcdeca09636dfd3e257016c31
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2025-06-12 14:19:25 -0300

    Bumsp linux image

--- test/setup/gitlab_vm.tf
+++ test/setup/gitlab_vm.tf
@@ -107,7 +107,7 @@ resource "google_compute_instance" "default" {
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
     }
   }
 
