82e54007575fc62507799d35579cadc01db7d334
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2025-10-15 14:11:04 -0300

    chore: removes commented code, extract gitclone function (#498)

--- helpers/eab-deployer/utils/git_test.go
+++ helpers/eab-deployer/utils/git_test.go
@@ -15,6 +15,7 @@
 package utils
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
 	"testing"
@@ -39,14 +40,80 @@ func createLocalRepo(t *testing.T, repo string) string {
 	return local
 }
 
-func TestGit(t *testing.T) {
+func TestGitCSR(t *testing.T) {
 	remote := "origin"
+	repo := createLocalRepo(t, "my-csr-git-repo")
+	originPath := filepath.Join(t.TempDir(), "my-csr-git-repo-origin")
+	err := CopyDirectory(repo, originPath)
+	assert.NoError(t, err)
+
+	// Test GitClone with CSR
+	localCSR := GitClone(t, "CSR", "my-csr-git-repo", repo, repo, "", logger.Discard)
+	assert.NotNil(t, localCSR)
+	err = localCSR.AddRemote(remote, originPath)
+	assert.NoError(t, err)
+
+	err = localCSR.CheckoutBranch("unit-test")
+	assert.NoError(t, err)
+
+	localBranch, err := localCSR.GetCurrentBranch()
+	assert.Equal(t, localBranch, "unit-test", "current branch should be 'unit-test'")
+	assert.NoError(t, err)
+
+	err = os.WriteFile(filepath.Join(repo, "go.mod"), []byte("module example.com/test\n"), 0644)
+	assert.NoError(t, err)
+
+	err = localCSR.CommitFiles("add go mod file")
+	assert.NoError(t, err)
+
+	err = localCSR.PushBranch("unit-test", remote)
+	assert.NoError(t, err)
+
+	hasUpstream, err := localCSR.HasUpstream("unit-test", remote)
+	assert.NoError(t, err)
+	assert.True(t, hasUpstream, "branch 'unit-test' should have a remote")
+
+	origin := cloneCSR(t, "my-csr-git-repo", originPath, "", logger.Discard)
+	files, err := FindFiles(originPath, "go.mod")
+	assert.NoError(t, err)
+	assert.Len(t, files, 0, "'go.mod' file should not exist on main branch")
+
+	err = origin.CheckoutBranch("unit-test")
+	assert.NoError(t, err)
+	files, err = FindFiles(originPath, "go.mod")
+	assert.NoError(t, err)
+	assert.Len(t, files, 1, "'go.mod' file should exist on 'unit-test' branch")
+
+	// Test CheckoutBranch when the branch already exists
+	err = localCSR.CheckoutBranch("unit-test")
+	assert.NoError(t, err)
+
+	// Test CommitFiles with no changes
+	err = localCSR.CommitFiles("no changes")
+	assert.NoError(t, err)
+
+	// Test GetCommitSha
+	_, err = localCSR.GetCommitSha()
+	assert.NoError(t, err)
+}
+
+func TestGitClone_NonCSR(t *testing.T) {
+	remote := "origin"
+	repoURL := "https://github.com/google/blueprint-test.git"
 	repo := createLocalRepo(t, "my-git-repo")
 	originPath := filepath.Join(t.TempDir(), "my-git-repo-origin")
 	err := CopyDirectory(repo, originPath)
 	assert.NoError(t, err)
 
-	local := CloneCSR(t, "my-git-repo", repo, "", logger.Discard)
+	local := GitClone(t, "GITHUB", "blueprint-test", repoURL, originPath, "", logger.Discard)
+	assert.NotNil(t, local)
+
+	// Check if the directory is created
+	if _, err := os.Stat(originPath); errors.Is(err, os.ErrNotExist) {
+		t.Fatalf("Directory not created: %s", originPath)
+	}
+
+	assert.NotNil(t, local)
 	err = local.AddRemote(remote, originPath)
 	assert.NoError(t, err)
 
@@ -69,15 +136,4 @@ func TestGit(t *testing.T) {
 	hasUpstream, err := local.HasUpstream("unit-test", remote)
 	assert.NoError(t, err)
 	assert.True(t, hasUpstream, "branch 'unit-test' should have a remote")
-
-	origin := CloneCSR(t, "my-git-repo", originPath, "", logger.Discard)
-	files, err := FindFiles(originPath, "go.mod")
-	assert.NoError(t, err)
-	assert.Len(t, files, 0, "'go.mod' file should not exist on main branch")
-
-	err = origin.CheckoutBranch("unit-test")
-	assert.NoError(t, err)
-	files, err = FindFiles(originPath, "go.mod")
-	assert.NoError(t, err)
-	assert.Len(t, files, 1, "'go.mod' file should exist on 'unit-test' branch")
 }
