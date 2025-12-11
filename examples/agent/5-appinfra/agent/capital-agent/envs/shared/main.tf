26c9df50e4c85eba7d262879f0069638442bb36a
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2025-12-11 09:14:21 -0300

    Removes model armor from agent example

--- examples/agent/5-appinfra/agent/capital-agent/envs/shared/main.tf
+++ examples/agent/5-appinfra/agent/capital-agent/envs/shared/main.tf
@@ -52,45 +52,6 @@ module "app" {
   binary_authorization_repository_id = data.terraform_remote_state.bootstrap.outputs.binary_authorization_repository_id
 }
 
-module "model_armor_configuration" {
-  source  = "GoogleCloudPlatform/vertex-ai/google//modules/model-armor-template"
-  version = "~> 2.3"
-
-  for_each    = local.cluster_projects_id
-  template_id = "ma-${local.application_name}-${local.service_name}"
-  location    = var.region
-  project_id  = each.value
-
-  rai_filters = {
-    dangerous         = "LOW_AND_ABOVE"
-    sexually_explicit = "MEDIUM_AND_ABOVE"
-  }
-
-  enable_malicious_uri_filter_settings = true
-
-  pi_and_jailbreak_filter_settings = "MEDIUM_AND_ABOVE"
-
-  sdp_settings = {
-    basic_config_filter_enforcement = true
-  }
-
-  metadata_configs = {
-    enforcement_type                         = "INSPECT_AND_BLOCK"
-    enable_multi_language_detection          = true
-    log_sanitize_operations                  = true
-    log_template_operations                  = true
-    ignore_partial_invocation_failures       = false
-    custom_prompt_safety_error_code          = "799"
-    custom_prompt_safety_error_message       = "error 799"
-    custom_llm_response_safety_error_message = "error 798"
-    custom_llm_response_safety_error_code    = "798"
-  }
-
-  labels = {
-    "agent" = "capital-agent"
-  }
-}
-
 resource "google_service_account" "gsa_capital_agent" {
   for_each     = local.cluster_projects_id
   project      = each.value
