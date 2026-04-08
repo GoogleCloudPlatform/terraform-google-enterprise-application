b0e48bd3b5bf0728994a93d7b2f66deb53d87fe0
Marcos Vinicius Leal da Silva <marcosvl@ciandt.com>
2026-04-08 18:53:18 -0300

    feat: harness example guide for EAB repository deployment

---
+++ docs/harness-example-guide/setup/kms.tf
@@ -0,0 +1,52 @@
+data "google_storage_project_service_account" "ci_gcs_account" {
+  project = module.seed_project.project_id
+}
+
+module "kms" {
+  source  = "terraform-google-modules/kms/google"
+  version = "~> 4.0"
+
+  project_id     = module.seed_project.project_id
+  location       = var.region
+  keyring        = "kms-bucket-encryption"
+  keys           = ["bucket"]
+  set_owners_for = ["bucket"]
+  owners = [
+    "serviceAccount:${var.cloud_build_sa}"
+  ]
+  set_encrypters_for = ["bucket"]
+  encrypters = [
+    "${data.google_storage_project_service_account.ci_gcs_account.member},serviceAccount:${var.cloud_build_sa}",
+  ]
+  set_decrypters_for = ["bucket"]
+  decrypters = [
+    "${data.google_storage_project_service_account.ci_gcs_account.member},serviceAccount:${var.cloud_build_sa}",
+  ]
+  prevent_destroy = false
+}
+
+module "kms_attestor" {
+  source  = "terraform-google-modules/kms/google"
+  version = "~> 4.0"
+
+  project_id          = module.seed_project.project_id
+  location            = var.region
+  keyring             = "kms-attestation-sign"
+  keys                = ["attestation"]
+  set_owners_for      = ["attestation"]
+  purpose             = "ASYMMETRIC_SIGN"
+  key_algorithm       = "RSA_SIGN_PKCS1_4096_SHA512"
+  key_rotation_period = null
+  owners = [
+    "serviceAccount:${var.cloud_build_sa}"
+  ]
+  set_encrypters_for = ["attestation"]
+  encrypters = [
+    "${data.google_storage_project_service_account.ci_gcs_account.member},serviceAccount:${var.cloud_build_sa}",
+  ]
+  set_decrypters_for = ["attestation"]
+  decrypters = [
+    "${data.google_storage_project_service_account.ci_gcs_account.member},serviceAccount:${var.cloud_build_sa}",
+  ]
+  prevent_destroy = false
+}
---
+++ docs/harness-example-guide/setup/main.tf
@@ -0,0 +1,139 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+resource "random_string" "prefix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+# Create mock seed folder
+module "folder_seed" {
+  source              = "terraform-google-modules/folders/google"
+  version             = "~> 5.0"
+  prefix              = random_string.prefix.result
+  parent              = "folders/${var.folder_id}"
+  names               = ["seed"]
+  deletion_protection = false
+}
+
+module "seed_project" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 18.0"
+
+  name                     = "ci-eab-seed"
+  random_project_id        = "true"
+  random_project_id_length = 4
+  org_id                   = var.org_id
+  folder_id                = module.folder_seed.id
+  billing_account          = var.billing_account
+  deletion_policy          = "DELETE"
+  default_service_account  = "KEEP"
+
+  activate_apis = [
+    "accesscontextmanager.googleapis.com",
+    "aiplatform.googleapis.com",
+    "anthos.googleapis.com",
+    "anthosconfigmanagement.googleapis.com",
+    "anthospolicycontroller.googleapis.com",
+    "apikeys.googleapis.com",
+    "artifactregistry.googleapis.com",
+    "binaryauthorization.googleapis.com",
+    "certificatemanager.googleapis.com",
+    "cloudbilling.googleapis.com",
+    "cloudbuild.googleapis.com",
+    "clouddeploy.googleapis.com",
+    "cloudfunctions.googleapis.com",
+    "cloudkms.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "cloudtrace.googleapis.com",
+    "compute.googleapis.com",
+    "container.googleapis.com",
+    "containeranalysis.googleapis.com",
+    "containerscanning.googleapis.com",
+    "gkehub.googleapis.com",
+    "iam.googleapis.com",
+    "iap.googleapis.com",
+    "mesh.googleapis.com",
+    "modelarmor.googleapis.com",
+    "monitoring.googleapis.com",
+    "multiclusteringress.googleapis.com",
+    "multiclusterservicediscovery.googleapis.com",
+    "networkmanagement.googleapis.com",
+    "networkservices.googleapis.com",
+    "notebooks.googleapis.com",
+    "orgpolicy.googleapis.com",
+    "secretmanager.googleapis.com",
+    "servicedirectory.googleapis.com",
+    "servicemanagement.googleapis.com",
+    "servicenetworking.googleapis.com",
+    "serviceusage.googleapis.com",
+    "sourcerepo.googleapis.com",
+    "sqladmin.googleapis.com",
+    "storage.googleapis.com",
+    "trafficdirector.googleapis.com",
+  ]
+
+  activate_api_identities = [
+    {
+      api = "compute.googleapis.com",
+      roles = [
+        "roles/compute.networkAdmin",
+        "roles/compute.admin"
+      ]
+    },
+    {
+      api = "cloudbuild.googleapis.com",
+      roles = [
+        "roles/cloudbuild.builds.builder",
+        "roles/cloudbuild.connectionAdmin",
+        "roles/cloudbuild.serviceAgent",
+      ]
+    },
+    {
+      api   = "workflows.googleapis.com",
+      roles = ["roles/workflows.serviceAgent"]
+    },
+    {
+      api   = "config.googleapis.com",
+      roles = ["roles/cloudconfig.serviceAgent"]
+    },
+    {
+      api   = "container.googleapis.com",
+      roles = ["roles/compute.networkAdmin", "roles/container.serviceAgent"]
+    },
+    {
+      api   = "gkehub.googleapis.com",
+      roles = ["roles/compute.networkAdmin", "roles/gkehub.serviceAgent"]
+    },
+    {
+      api   = "artifactregistry.googleapis.com",
+      roles = ["roles/artifactregistry.serviceAgent"]
+    },
+    {
+      api   = "storage.googleapis.com",
+      roles = []
+    },
+    {
+      api   = "networkservices.googleapis.com",
+      roles = []
+    },
+    {
+      api   = "aiplatform.googleapis.com",
+      roles = ["roles/aiplatform.serviceAgent"]
+    }
+  ]
+}
