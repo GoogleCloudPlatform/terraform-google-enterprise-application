1937796045e4c16bc076f10b2f065101da946f00
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2026-04-22 14:11:50 -0300

    Bump bootstrap to 12.0

--- 1-bootstrap/main.tf
+++ 1-bootstrap/main.tf
@@ -55,7 +55,7 @@ module "cloudbuild_repositories" {
   count = local.use_csr ? 0 : 1
 
   source  = "terraform-google-modules/bootstrap/google//modules/cloudbuild_repo_connection"
-  version = "~> 11.0"
+  version = "~> 12.0"
 
   project_id = var.project_id
 
@@ -103,7 +103,7 @@ module "tf_cloudbuild_workspace" {
   for_each = var.cloudbuildv2_repository_config.repositories
 
   source  = "terraform-google-modules/bootstrap/google//modules/tf_cloudbuild_workspace"
-  version = "~> 11.0"
+  version = "~> 12.0"
 
   project_id = var.project_id
   location   = var.location
