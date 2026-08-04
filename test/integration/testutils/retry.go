f1f6feee6270f4c66fe28d2ecbdab67dcca892fe
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2026-08-04 11:29:04 -0300

    Apply suggestions from code review
    Co-authored-by: Amanda Karina Lopes de Oliveira <amandak@ciandt.com>

--- test/integration/testutils/retry.go
+++ test/integration/testutils/retry.go
@@ -76,6 +76,7 @@ var (
 		".*Error 400: Feature has associated resources that should be cleaned up before deletion.*": "Waiting Fleetscope service cleanup",
 
 		".*Error waiting for deleting GKE cluster.*": "Error waiting for deleting GKE cluster: Calling fleet operation service DeleteMembership, please ensure fleet service account has access to the project.",
+     ".*Could not connect to server.*": "Network error.",
 	}
 
 	RetryableDeploymentErrors = map[string]string{
