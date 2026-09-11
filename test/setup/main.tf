dfcf7b520916ecc35b455ed77ab8692a6595767f
gabrielmm <gabrielmm@ciandt.com>
2026-09-11 11:39:58 -0300

    feat: replace legacy boolean-based ternary logic with declarative examples_namespaces map in test/setup/main.tf.
    Co-authored-by: Amanda Karina Lopes de Oliveira <amandakarina@users.noreply.github.com>

--- test/setup/main.tf
+++ test/setup/main.tf
@@ -15,12 +15,16 @@
  */
 
 locals {
-  teams = var.hpc ? ["hpc-team-a", "hpc-team-b"] : var.agent ? ["capital-agent", "vllm-model"] : setunion([
-    "cb-frontend",
-    "cb-accounts",
-    "cb-ledger"],
-    !var.single_project ? ["cymbalshops"] : []
-  )
+
+  examples_namespaces = {
+    "llm-model"       = ["vllm-model"],
+    "agent"           = ["capital-agent"],
+    "cymbal-bank"     = ["cb-frontend", "cb-accounts", "cb-ledger"],
+    "default-example" = [],
+    "cymbal-shop"     = ["cymbalshops"],
+  }
+
+  teams = distinct(flatten([for i in var.examples_tested : lookup(local.examples_namespaces, i, [])]))
 }
 
 resource "random_string" "prefix" {
